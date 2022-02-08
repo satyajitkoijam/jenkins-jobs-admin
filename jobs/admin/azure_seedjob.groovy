6b87b85ef42b9b7abce1a572f909dabfeed1ba9d
Satyajit Koijam <satyajitkoijam@gmail.com>
2022-02-08 17:04:10 +0530

    updating pipeline

--- jobs/admin/azure_seedjob.groovy
+++ jobs/admin/azure_seedjob.groovy
@@ -1,1 +1,23 @@
 def jobDslScript = 'pipelines/admin/jobDsl.groovy'
+def codeGitRepo  = ['https://github.com/satyajitkoijam/jenkins-repo.git']
+def codeGitBranch = ['master']
+
+def scripts = [
+    "jobs/*.groovy"
+]
+
+pipelineJob('azure-seedjob') {
+    description('')
+    logRotator(5, 5)
+    parameters {
+        choiceParam('gitUrl', codeGitRepo, '')
+        choiceParam('gitBranch', codeGitBranch, '')
+        choiceParam('dslScript', scripts, '')
+    }
+    definition {
+        cps {
+            scripts(readFileFromWorkspace(jobDslScript))
+            sandbox()
+        }
+    }
+}
